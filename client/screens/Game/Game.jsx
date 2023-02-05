c0c5cf25ecb38b3e14346c3bb47f2fdeabe3c6e6
stustr <sfbsturrock@gmail.com>
2023-02-05 18:49:13 +0000

    added GameLogic to Game

---
+++ client/screens/Game/Game.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import GameLogic from "./GameLogic";
+
+const Game = () => {
+  return (
+    <View>
+      <GameLogic />
+    </View>
+  );
+};
+export default Game;
