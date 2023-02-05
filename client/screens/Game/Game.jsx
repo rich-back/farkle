8ace460866a372d38cca4a8d543bdeb7e4d61bb4
stustr <sfbsturrock@gmail.com>
2023-02-05 22:37:27 +0000

    initialized score and dice as state in Game

--- client/screens/Game/Game.jsx
+++ client/screens/Game/Game.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import { View, Text } from "react-native";
 import GameLogic from "./GameLogic";
+import ScoringLogic from './ScoringLogic';
 
 const Game = () => {
+
+  const [score, setScore] = useState(10000)
+  const [rollingScore, setRollingScore] = useState(0)
+  const [liveDice, setLiveDice] = useState()
+  const [keptDice, setKeptDice] = useState()
+
+  const [dice, setDice] = useState([
+    { key: "dice1", value: 1 },
+    { key: "dice2", value: 2 },
+    { key: "dice3", value: 3 },
+    { key: "dice4", value: 4 },
+    { key: "dice5", value: 5 },
+    { key: "dice6", value: 6 },
+  ]);
+
   return (
     <View>
-      <GameLogic />
+      <GameLogic dice={dice} setDice={setDice} liveDice={liveDice} keptDice={keptDice}/>
+      <ScoringLogic dice={dice} setDice={setDice} liveDice={liveDice} setLiveDice={setLiveDice} keptDice={keptDice} setKeptDice={setKeptDice} score={score} setScore={setScore} rollingScore={rollingScore} setRollingScore={setRollingScore}/>
+      <Text>Score {score}</Text>
+      <Text>Rolling Score {rollingScore}</Text>
     </View>
   );
 };
