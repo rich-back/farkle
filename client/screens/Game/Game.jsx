e9edebc9087153f3dab9d649fba18f8b38e40561
stustr <sfbsturrock@gmail.com>
2023-02-06 13:13:09 +0000

    added Farkle message Modal

--- client/screens/Game/Game.jsx
+++ client/screens/Game/Game.jsx
@@ -1,15 +1,14 @@
 import { useState } from "react";
-import { Text, View } from "react-native";
+import { Button, Modal, Pressable, Text, View } from "react-native";
 import GameLogic from "./GameLogic";
 import ScoringLogic from "./ScoringLogic";
 
 const Game = () => {
-  const [score, setScore] = useState(10000);
-  const [roundScore, setRoundScore] = useState(0);
   const [liveDice, setLiveDice] = useState();
   const [keptDice, setKeptDice] = useState();
   const [counted, setCounted] = useState(false);
   const [turnCounter, setTurnCounter] = useState(1);
+  const [farkleAlertVis, setFarkleAlertVis] = useState(false);
   const [dice, setDice] = useState([
     { key: "dice1", value: 1 },
     { key: "dice2", value: 2 },
@@ -21,37 +20,45 @@ const Game = () => {
 
   const keepDi = () => {
     setKeptDice();
-  }
-
+  };
 
   return (
     <View>
+      <Modal
+        animationType="fade"
+        presentationStyle="overFullScreen"
+        transparent={true}
+        visible={farkleAlertVis}
+      >
+        <View>
+          <Text>Farkle</Text>
+          <Pressable onPress={() => setFarkleAlertVis(false)}>
+            <Text>Close modal</Text>
+          </Pressable>
+        </View>
+      </Modal>
       <GameLogic
+        counted={counted}
         dice={dice}
-        keepDi = {keepDi}
+        keepDi={keepDi}
         keptDice={keptDice}
         liveDice={liveDice}
         setCounted={setCounted}
         setDice={setDice}
+        setFarkleAlertVis={setFarkleAlertVis}
       />
       <ScoringLogic
         counted={counted}
         dice={dice}
         keptDice={keptDice}
         liveDice={liveDice}
-        roundScore={roundScore}
-        score={score}
         turnCounter={turnCounter}
         setCounted={setCounted}
         setDice={setDice}
         setKeptDice={setKeptDice}
         setLiveDice={setLiveDice}
-        setRoundScore={setRoundScore}
-        setScore={setScore}
         setTurnCounter={setTurnCounter}
       />
-      <Text>Score: {score}</Text>
-      <Text>Round Score: {roundScore}</Text>
     </View>
   );
 };
