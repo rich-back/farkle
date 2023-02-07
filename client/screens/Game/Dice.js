d5a3508e0367a82b5c4d24facd74e65ab59e4245
stustr <sfbsturrock@gmail.com>
2023-02-07 21:55:19 +0000

    :recycle: Dice.js added to house dice array

---
+++ client/screens/Game/Dice.js
@@ -0,0 +1,10 @@
+const dice = [
+  { key: "dice1", value: 1 },
+  { key: "dice2", value: 2 },
+  { key: "dice3", value: 3 },
+  { key: "dice4", value: 4 },
+  { key: "dice5", value: 5 },
+  { key: "dice6", value: 6 },
+];
+
+export default dice;
