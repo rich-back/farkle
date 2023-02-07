1dafcb82ecbcce57b63da43754c7a2b6536e56b0
rich-back <backhouse.richard@gmail.com>
2023-02-07 12:25:24 +0000

    added Screens routing to App.js

--- client/App.js
+++ client/App.js
@@ -3,14 +3,20 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Game from "./screens/Game/Game";
+import Home from "./screens/Home/Home";
+import LeaderBoard from "./screens/LeaderBoard/LeaderBoard";
+import Instructions from "./screens/Instructions/Instructions";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Game" component={Game} />
+        <Stack.Screen name="LeaderBoard" component={LeaderBoard} />
+        <Stack.Screen name="Instructions" component={Instructions} />
       </Stack.Navigator>
     </NavigationContainer>
   );
