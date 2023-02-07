b32b20f93f5c5bad7b9114f79521f5598e33fc69
rich-back <backhouse.richard@gmail.com>
2023-02-07 14:49:04 +0000

    added SplashScreen.jsx

---
+++ client/screens/SplashScreen/SplashScreen.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SplashScreen = () => {
+  return (
+    <View>
+      <Text>SplashScreen</Text>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({})
+
+export default SplashScreen
