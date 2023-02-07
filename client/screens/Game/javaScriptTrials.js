9034511b72b02ddc43f85c37593ddbdcc3533977
stustr <sfbsturrock@gmail.com>
2023-02-07 22:02:35 +0000

    jstrials now js

---
+++ client/screens/Game/javaScriptTrials.js
@@ -0,0 +1,8 @@
+export default function GamingLogic() {
+    
+    const getRandomNumber = () => {
+        const randomNumber = Math.floor(Math.random() * 6) + 1;
+        return randomNumber;
+      };
+      
+}
