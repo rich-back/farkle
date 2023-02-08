f161216afb01b1768d6c0a0f6dd9055e363052e9
rich-back <backhouse.richard@gmail.com>
2023-02-08 16:50:21 +0000

    ✅ test scores all variants of 3 of a kind

--- client/screens/Game/Scoring/ScoringLogic.test.js
+++ client/screens/Game/Scoring/ScoringLogic.test.js
@@ -11,10 +11,50 @@ const dice = [
 
 const diceThreeOfAKind = [
     { key: "dice1", value: 1 },
+    { key: "dice2", value: 1 },
+    { key: "dice3", value: 1 },
+    { key: "dice4", value: 5 },
+    { key: "dice5", value: 5 },
+    { key: "dice6", value: 6 },
+  ];
+const diceThreeOfAKind2 = [
+    { key: "dice1", value: 1 },
+    { key: "dice2", value: 2 },
+    { key: "dice3", value: 2 },
+    { key: "dice4", value: 2 },
+    { key: "dice5", value: 5 },
+    { key: "dice6", value: 6 },
+  ];
+const diceThreeOfAKind3 = [
+    { key: "dice1", value: 1 },
+    { key: "dice2", value: 2 },
+    { key: "dice3", value: 3 },
+    { key: "dice4", value: 3 },
+    { key: "dice5", value: 3 },
+    { key: "dice6", value: 6 },
+  ];
+const diceThreeOfAKind4 = [
+    { key: "dice1", value: 1 },
+    { key: "dice2", value: 2 },
+    { key: "dice3", value: 4 },
+    { key: "dice4", value: 4 },
+    { key: "dice5", value: 4 },
+    { key: "dice6", value: 6 },
+  ];
+const diceThreeOfAKind5 = [
+    { key: "dice1", value: 1 },
     { key: "dice2", value: 2 },
     { key: "dice3", value: 5 },
     { key: "dice4", value: 5 },
     { key: "dice5", value: 5 },
+    { key: "dice6", value: 6 },
+  ];
+const diceThreeOfAKind6 = [
+    { key: "dice1", value: 1 },
+    { key: "dice2", value: 2 },
+    { key: "dice3", value: 5 },
+    { key: "dice4", value: 6 },
+    { key: "dice5", value: 6 },
     { key: "dice6", value: 6 },
   ];
 
@@ -89,4 +129,24 @@ const diceThreeOfAKind = [
   it('scores five of a kind', () => {
     let expected = countScore({roundScore:0,rollScore:0, keptDice: diceFiveOfAKind})
     expect(expected.roundScore).toBe(2000)
+  })
+
+  it('scores four of a kind', () => {
+    let expected = countScore({roundScore:0,rollScore:0, keptDice: diceFourOfAKind})
+    expect(expected.roundScore).toBe(1000)
+  })
+
+  it('scores all variants of three of a kind', () => {
+    let expected1 = countScore({roundScore:0,rollScore:0, keptDice: diceThreeOfAKind})
+    let expected2 = countScore({roundScore:0,rollScore:0, keptDice: diceThreeOfAKind2})
+    let expected3 = countScore({roundScore:0,rollScore:0, keptDice: diceThreeOfAKind3})
+    let expected4 = countScore({roundScore:0,rollScore:0, keptDice: diceThreeOfAKind4})
+    let expected5 = countScore({roundScore:0,rollScore:0, keptDice: diceThreeOfAKind5})
+    let expected6 = countScore({roundScore:0,rollScore:0, keptDice: diceThreeOfAKind6})
+    expect(expected1.roundScore).toBe(100)
+    expect(expected2.roundScore).toBe(200)
+    expect(expected3.roundScore).toBe(300)
+    expect(expected4.roundScore).toBe(400)
+    expect(expected5.roundScore).toBe(500)
+    expect(expected6.roundScore).toBe(600)
   })
