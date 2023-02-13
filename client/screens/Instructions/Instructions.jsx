a7e8a0f8d7ccc77917287d2d54419d2a7c04d6ba
stustr <sfbsturrock@gmail.com>
2023-02-13 18:28:13 +0000

    🚚 rejigged assets folder so images are in own folder

--- client/screens/Instructions/Instructions.jsx
+++ client/screens/Instructions/Instructions.jsx
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
 
-import scoringRules from '../../assets/farkle-scoresheet.png'
+import scoringRules from '../../assets/images/farkle-scoresheet.png'
 
 const Instructions = () => {
   return (
