fc7210418554e0bd9ad617576701a6d03adf6c10
rich-back <backhouse.richard@gmail.com>
2023-04-23 11:31:52 +0100

    💄 Modal styling/dynamic content updates 📱 Screen layout improved responsiveness

--- client/screens/Game/GameScreen.jsx
+++ client/screens/Game/GameScreen.jsx
@@ -12,7 +12,6 @@ import {
 } from "react-native";
 import background from "../../assets/images/background.png";
 import farkleModal from "../../assets/modals/farkle-modal.png";
-import winnerModal from "../../assets/modals/winner-modal2.png";
 import { GameTypeContext } from "../../global/GameContext";
 import { dice } from "../../components/Dice";
 import GameLogicScreen from "../../components/GamingLogic/GamingLogicComponent";
@@ -20,6 +19,7 @@ import ScoringScreen from "../../components/Scoring/ScoringComponent";
 import arrowL from "../../assets/images/arrow.png";
 import arrowR from "../../assets/images/arrowR.png";
 import BackButton from "../../components/BackButton";
+import winnerModal from "../../assets/modals/post-it-blankL.png";
 
 const Game = ({ navigation }) => {
   const {
@@ -75,8 +75,8 @@ const Game = ({ navigation }) => {
   const handleEndGame = () => {
     setEndGameAlertVis(false);
     setCurrentPlayer(player1);
-    setPlayer1({ ...player1, score: 500 });
-    setPlayer2({ ...player2, score: 500 });
+    setPlayer1({ ...player1, score: player1.score });
+    setPlayer2({ ...player2, score: player2.score });
   };
 
   return (
@@ -85,137 +85,172 @@ const Game = ({ navigation }) => {
       resizeMode="cover"
       style={styles.background}
     >
-      <SafeAreaView className="h-full flex-1 justify-between mr-5 ml-5 mt-3">
-        <View className="flex flex-row justify-between">
-          {currentPlayer.playerName == player1.playerName ? (
-            <View className="">
-              {/* <Image className="absolute z-0" source={arrowL} /> */}
-              <Text className="font-virgil text-2xl">
-                {player1.playerName}:{" "}
-              </Text>
-              <Text className="font-virgil text-2xl">{player1.score}</Text>
-            </View>
-          ) : (
-            <View>
-              <Text className="font-virgil text-gray-400 text-lg">
-                {player1.playerName}:
-              </Text>
-              <Text className="font-virgil text-gray-400 text-lg">
-                {player1.score}
-              </Text>
-            </View>
-          )}
+      <SafeAreaView className="h-screen flex-1 justify-between mr-5 ml-5 mt-3">
+        <View className="flex-1 justify-evenly">
+          <View className="flex-none">
+            <View className="flex-row justify-between">
+              {currentPlayer.playerName == player1.playerName ? (
+                <View className="">
+                  {/* <Image className="absolute z-0" source={arrowL} /> */}
+                  <Text className="font-virgil text-2xl">
+                    {player1.playerName}:{" "}
+                  </Text>
+                  <Text className="font-virgil text-2xl">{player1.score}</Text>
+                </View>
+              ) : (
+                <View>
+                  <Text className="font-virgil text-gray-400 text-lg">
+                    {player1.playerName}:
+                  </Text>
+                  <Text className="font-virgil text-gray-400 text-lg">
+                    {player1.score}
+                  </Text>
+                </View>
+              )}
 
-
-          {currentPlayer.playerName == player2.playerName ? (
-            <View>
-              {/* <Image className="absolute right-8 z-0" source={arrowR} /> */}
-              <Text className="font-virgil text-2xl">
-                {player2.playerName}:
-              </Text>
-              <Text className="font-virgil text-2xl">{player2.score}</Text>
+              {currentPlayer.playerName == player2.playerName ? (
+                <View>
+                  {/* <Image className="absolute right-8 z-0" source={arrowR} /> */}
+                  <Text className="font-virgil text-2xl">
+                    {player2.playerName}:
+                  </Text>
+                  <Text className="font-virgil text-2xl">{player2.score}</Text>
+                </View>
+              ) : (
+                <View>
+                  <Text className="font-virgil text-gray-400 text-lg">
+                    {player2.playerName}:
+                  </Text>
+                  <Text className="font-virgil text-gray-400 text-lg">
+                    {player2.score}
+                  </Text>
+                </View>
+              )}
             </View>
-          ) : (
-            <View>
-              <Text className="font-virgil text-gray-400 text-lg">
-                {player2.playerName}:
-              </Text>
-              <Text className="font-virgil text-gray-400 text-lg">
-                {player2.score}
+
+            <View className="m-4">
+              <Text className="font-virgil text-2xl text-center">
+                Round Score: {roundScore}
               </Text>
             </View>
-          )}
-        </View>
-
-          <View className="m-4">
-            <Text className="font-virgil text-2xl text-center">
-              Round Score: {roundScore}
-            </Text>
+          </View>
+          <View className="grow">
+            <GameLogicScreen
+              bankedDice={bankedDice}
+              counted={counted}
+              disabled={disabled}
+              hasSelectedDice={hasSelectedDice}
+              keptDice={keptDice}
+              liveDice={liveDice}
+              playSound={playSound}
+              rollScore={rollScore}
+              roundScore={roundScore}
+              setBankedDice={setBankedDice}
+              setCounted={setCounted}
+              setDisabled={setDisabled}
+              setEndable={setEndable}
+              setFarkleAlertVis={setFarkleAlertVis}
+              setHasSelectedDice={setHasSelectedDice}
+              setKeptDice={setKeptDice}
+              setLiveDice={setLiveDice}
+              setRollScore={setRollScore}
+              setRoundScore={setRoundScore}
+            />
+          </View>
+          <View className="flex-none">
+            <ScoringScreen
+              bankedDice={bankedDice}
+              counted={counted}
+              disabled={disabled}
+              endable={endable}
+              hasSelectedDice={hasSelectedDice}
+              keptDice={keptDice}
+              liveDice={liveDice}
+              playSound={playSound}
+              rollScore={rollScore}
+              roundScore={roundScore}
+              setBankedDice={setBankedDice}
+              setCounted={setCounted}
+              setDisabled={setDisabled}
+              setEndable={setEndable}
+              setEndGameAlertVis={setEndGameAlertVis}
+              setHasSelectedDice={setHasSelectedDice}
+              setKeptDice={setKeptDice}
+              setLiveDice={setLiveDice}
+              setRollScore={setRollScore}
+              setRoundScore={setRoundScore}
+              setTurnCounter={setTurnCounter}
+              turnCounter={turnCounter}
+            />
           </View>
 
-        <View className="flex-6">
-          <GameLogicScreen
-            bankedDice={bankedDice}
-            counted={counted}
-            disabled={disabled}
-            hasSelectedDice={hasSelectedDice}
-            keptDice={keptDice}
-            liveDice={liveDice}
-            playSound={playSound}
-            rollScore={rollScore}
-            roundScore={roundScore}
-            setBankedDice={setBankedDice}
-            setCounted={setCounted}
-            setDisabled={setDisabled}
-            setEndable={setEndable}
-            setFarkleAlertVis={setFarkleAlertVis}
-            setHasSelectedDice={setHasSelectedDice}
-            setKeptDice={setKeptDice}
-            setLiveDice={setLiveDice}
-            setRollScore={setRollScore}
-            setRoundScore={setRoundScore}
-          />
-        </View>
-        <View className="flex-1">
-          <ScoringScreen
-            bankedDice={bankedDice}
-            counted={counted}
-            disabled={disabled}
-            endable={endable}
-            hasSelectedDice={hasSelectedDice}
-            keptDice={keptDice}
-            liveDice={liveDice}
-            playSound={playSound}
-            rollScore={rollScore}
-            roundScore={roundScore}
-            setBankedDice={setBankedDice}
-            setCounted={setCounted}
-            setDisabled={setDisabled}
-            setEndable={setEndable}
-            setEndGameAlertVis={setEndGameAlertVis}
-            setHasSelectedDice={setHasSelectedDice}
-            setKeptDice={setKeptDice}
-            setLiveDice={setLiveDice}
-            setRollScore={setRollScore}
-            setRoundScore={setRoundScore}
-            setTurnCounter={setTurnCounter}
-            turnCounter={turnCounter}
-          />
-        </View>
+          <Modal
+            animationType="slide"
+            presentationStyle="overFullScreen"
+            transparent={true}
+            visible={farkleAlertVis}
+          >
+            <View className="flex-1 h-full w-full self-center justify-center items-center">
+              <Pressable onPress={() => setFarkleAlertVis(false)}>
+                <Image
+                  source={farkleModal}
+                  className="w-screen"
+                  style={{ resizeMode: "contain" }}
+                />
+              </Pressable>
+            </View>
+          </Modal>
 
-        <Modal
-          animationType="slide"
-          presentationStyle="overFullScreen"
-          transparent={true}
-          visible={farkleAlertVis}
-        >
-          <View className="flex-1 h-full w-full self-center justify-center items-center">
-            <Pressable onPress={() => setFarkleAlertVis(false)}>
+          <Modal
+            animationType="slide"
+            presentationStyle="overFullScreen"
+            transparent={true}
+            visible={endGameAlertVis}
+          >
+            <View className="flex-1 h-full w-full self-center justify-center items-center">
               <Image
-                source={farkleModal}
-                className="w-[350px]"
+                source={winnerModal}
+                className="w-screen"
                 style={{ resizeMode: "contain" }}
               />
-            </Pressable>
-          </View>
-        </Modal>
+            </View>
 
-        <Modal
-          animationType="slide"
-          presentationStyle="overFullScreen"
-          transparent={true}
-          visible={endGameAlertVis}
-        >
-          <View className="flex-1 h-full w-full self-center justify-center items-center">
-            <Pressable
-              onPress={() => {
-                handleEndGame();
-              }}
-            >
-              <Image source={winnerModal} className="w-[350px]" style={{resizeMode: 'contain'}}/>
-            </Pressable>
-          </View>
-        </Modal>
+            <View className="flex-1 h-full absolute self-center justify-center mx-6 space-y-3">
+              <View className="space-y-1">
+                <Text className="font-virgil text-red-600 text-4xl scale-150 text-center skew-y-1 shadow-white">
+                  {currentPlayer.playerName}
+                </Text>
+                <Text className="font-virgil text-red-600 text-4xl text-center">
+                  is the
+                </Text>
+                <Text className="font-virgil text-red-600 text-7xl text-center -skew-y-3">
+                  Winner!
+                </Text>
+              </View>
+
+              <View className="space-y-4">
+                <Pressable
+                  onPress={() => {
+                    handleEndGame();
+                  }}
+                >
+                  <Text className="font-virgil text-green-600 text-4xl text-center">
+                    Play again!
+                  </Text>
+                </Pressable>
+                <Pressable
+                  onPress={() => {
+                    navigation.navigate("Home");
+                  }}
+                >
+                  <Text className="font-virgil text-2xl text-center">
+                    Take me Home.
+                  </Text>
+                </Pressable>
+              </View>
+            </View>
+          </Modal>
+        </View>
       </SafeAreaView>
     </ImageBackground>
   );
@@ -225,44 +260,6 @@ const styles = StyleSheet.create({
   background: {
     width: "100%",
     height: "100%",
-  },
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22,
-    opacity: 80,
-    elevation: 50,
-  },
-  modalView: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 250,
-    marginBottom: 250,
-    backgroundColor: "transparent",
-    width: 350,
-    height: 200,
-    borderRadius: 15,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  modalImage: {
-    elevation: 9,
-  },
-
-  modalButton: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    marginTop: 20,
   },
 });
 
